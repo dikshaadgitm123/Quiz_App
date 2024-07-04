b0263fe0e344b96ef9a50ac596f4a70c53ad1569
dikshaadgitm123 <gargdiksha177@gmail.com>
2024-07-04 16:47:25 +0530

    Quiz-App

---
+++ js/question.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Which device is required for the internet connection",
+  answer: "Modem",
+  options: [
+    "Modem",
+    "Pen Drive",
+    "Internet",
+    "LAN Cable"
+  ]
+},
+  {
+  numb: 2,
+  question: "Junk e-mail is also called?",
+  answer: "Spam",
+  options: [
+    "Fake",
+    "Bin",
+    "Spam",
+    "Archived"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which CSS property is used to control the spacing between elements?",
+  answer: "padding",
+  options: [
+    "margin",
+    "spacing",
+    "padding",
+    "border-spacing"
+  ]
+},
+  {
+  numb: 4,
+  question: "In React.js, which hook is used to perform side effects in a function component?",
+  answer: "useEffect",
+  options: [
+    "useState",
+    "useEffect",
+    "useReducer",
+    "useContext"
+  ]
+},
+  {
+  numb: 5,
+  question: "A computer cannot BOOT if it does not have the?",
+  answer: "Operating System",
+  options: [
+    "Internet",
+    "Operating System",
+    "Mouse",
+    "Application Software"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
